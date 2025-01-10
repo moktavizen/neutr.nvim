30acce978bed7969c7aa620305ba065120caf9eb
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-01-10 09:16:53 +0700

    Separate dev theme

--- shipwright_build.lua
+++ shipwright_build.lua
@@ -5,11 +5,11 @@
 ---@diagnostic disable: undefined-global
 local lushwright = require("shipwright.transform.lush")
 run(
-	require("lua.lush_theme.neutr"),
+	require("lua.lush_theme.neutr-dev"),
 	-- generate lua code
 	lushwright.to_lua,
 	-- write the lua code into our destination.
 	-- you must specify open and close markers yourself to account
 	-- for differing comment styles, patchwrite isn't limited to lua files.
-	{ patchwrite, "colors/colorscheme.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
+	{ patchwrite, "colors/neutr.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
 )
