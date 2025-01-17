f6c5f6fa37e86c7e2ce5d0d585f6f58701022643
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-01-17 16:53:11 +0700

    Add lualine for dev theme

---
+++ lua/lualine/themes/neutr-dev.lua
@@ -0,0 +1,28 @@
+local colors = {
+	blue = "#97b4f2",
+	green = "#cdf297",
+	black = "#212121",
+	white = "#ececec",
+	red = "#f2979c",
+	purple = "#cd97f2",
+	grey = "#383838",
+	light_grey = "#888888",
+}
+
+return {
+	normal = {
+		a = { gui = "bold", fg = colors.black, bg = colors.blue },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.light_grey, bg = colors.black },
+	},
+
+	insert = { a = { gui = "bold", fg = colors.black, bg = colors.green } },
+	visual = { a = { gui = "bold", fg = colors.black, bg = colors.purple } },
+	replace = { a = { gui = "bold", fg = colors.black, bg = colors.red } },
+
+	inactive = {
+		a = { fg = colors.light_grey, bg = colors.black },
+		b = { fg = colors.light_grey, bg = colors.black },
+		c = { fg = colors.light_grey, bg = colors.black },
+	},
+}
