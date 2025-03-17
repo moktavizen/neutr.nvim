3641e6c043bee68886cef75190df79af1971f120
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-03-17 16:19:47 +0700

    specify lualine colors

--- lua/lualine/themes/neutr.lua
+++ lua/lualine/themes/neutr.lua
@@ -1,33 +1,37 @@
 local colors = {
 	blue = "#97aef2",
 	green = "#c4f297",
-	black = "#1f1f1f",
-	white = "#ebebeb",
 	red = "#f29797",
 	magenta = "#c497f2",
 	grey = "#373737",
 	light_grey = "#868686",
+	black = "#1f1f1f",
+	white = "#ebebeb",
 }
 
 return {
 	normal = {
-		a = { gui = "bold", fg = colors.black, bg = colors.blue },
+		a = { fg = colors.black, bg = colors.blue, gui = "bold" },
 		b = { fg = colors.white, bg = colors.grey },
 		c = { fg = colors.light_grey, bg = colors.black },
 	},
-
-	insert = { a = { gui = "bold", fg = colors.black, bg = colors.green } },
-	visual = { a = { gui = "bold", fg = colors.black, bg = colors.magenta } },
-	replace = { a = { gui = "bold", fg = colors.black, bg = colors.red } },
-
-	command = {
-		a = { fg = colors.whte, bg = colors.black },
-		b = { fg = colors.whte, bg = colors.black },
-		c = { fg = colors.whte, bg = colors.black },
+	insert = {
+		a = { fg = colors.black, bg = colors.green, gui = "bold" },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.light_grey, bg = colors.black },
 	},
-
+	visual = {
+		a = { fg = colors.black, bg = colors.magenta, gui = "bold" },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.light_grey, bg = colors.black },
+	},
+	replace = {
+		a = { fg = colors.black, bg = colors.red, gui = "bold" },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.light_grey, bg = colors.black },
+	},
 	inactive = {
-		a = { fg = colors.light_grey, bg = colors.black },
+		a = { fg = colors.light_grey, bg = colors.black, gui = "bold" },
 		b = { fg = colors.light_grey, bg = colors.black },
 		c = { fg = colors.light_grey, bg = colors.black },
 	},
