aed5af7a3d6731335614bff5dd795b7f7d840fbd
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-08-01 15:17:29 +0700

    feat: improve statusline a11y

--- lua/lualine/themes/neutr.lua
+++ lua/lualine/themes/neutr.lua
@@ -3,28 +3,28 @@ local c = require('neutr.palette.dark')
 -- stylua: ignore
 return {
   normal   = {
-    a      = { fg = c.neutral.s900, bg = c.blue.s300,    gui = 'bold' },
-    b      = { fg = c.neutral.s50,  bg = c.neutral.s700               },
-    c      = { fg = c.neutral.s300, bg = c.neutral.s900               },
-  },
+    a      = { fg = c.blue.s300,    bg = c.neutral.s900, gui = 'bold' },
+    b      = { fg = c.neutral.s50,  bg = c.neutral.s900               },
+    c      = { fg = c.neutral.s200, bg = c.neutral.s900               },
+                                                                      },
   insert   = {
-    a      = { fg = c.neutral.s900, bg = c.green.s300,   gui = 'bold' },
-    b      = { fg = c.neutral.s50,  bg = c.neutral.s700               },
-    c      = { fg = c.neutral.s300, bg = c.neutral.s900               },
-  },
+    a      = { fg = c.green.s300,   bg = c.neutral.s900, gui = 'bold' },
+    b      = { fg = c.neutral.s50,  bg = c.neutral.s900               },
+    c      = { fg = c.neutral.s200, bg = c.neutral.s900               },
+                                                                      },
   visual   = {
-    a      = { fg = c.neutral.s900, bg = c.magenta.s300, gui = 'bold' },
-    b      = { fg = c.neutral.s50,  bg = c.neutral.s700               },
-    c      = { fg = c.neutral.s300, bg = c.neutral.s900               },
-  },
+    a      = { fg = c.magenta.s300, bg = c.neutral.s900, gui = 'bold' },
+    b      = { fg = c.neutral.s50,  bg = c.neutral.s900               },
+    c      = { fg = c.neutral.s200, bg = c.neutral.s900               },
+                                                                      },
   replace  = {
-    a      = { fg = c.neutral.s900, bg = c.red.s300,     gui = 'bold' },
-    b      = { fg = c.neutral.s50,  bg = c.neutral.s700               },
-    c      = { fg = c.neutral.s300, bg = c.neutral.s900               },
-  },
+    a      = { fg = c.red.s300,     bg = c.neutral.s900, gui = 'bold' },
+    b      = { fg = c.neutral.s50,  bg = c.neutral.s900               },
+    c      = { fg = c.neutral.s200, bg = c.neutral.s900               },
+                                                                      },
   inactive = {
-    a      = { fg = c.neutral.s300, bg = c.neutral.s900, gui = 'bold' },
-    b      = { fg = c.neutral.s300, bg = c.neutral.s900               },
-    c      = { fg = c.neutral.s300, bg = c.neutral.s900               },
-  },
+    a      = { fg = c.neutral.s600, bg = c.neutral.s900, gui = 'bold' },
+    b      = { fg = c.neutral.s600, bg = c.neutral.s900               },
+    c      = { fg = c.neutral.s600, bg = c.neutral.s900               },
+                                                                      },
 }
